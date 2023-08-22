0a66ba9b26c2b809b2958d68285dfd2d27cb9a7f
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2023-08-22 17:16:08 +0500

    speech recognition

--- voice_recognition.py
+++ voice_recognition.py
@@ -1,1 +1,23 @@
 # I typed a command in terminal name "pip3 install speechrecognition"
+#  then I used a library "pip3 install pyttsx3" and "________________ pyaudio"
+
+import speech_recognition
+import pyttsx3
+import pyaudio
+import string
+
+Recognizer = speech_recognition.Recognizer()
+# What you exactly saying in the microphone.
+while True:
+    try:
+        with speech_recognition.Microphone() as mic:
+            Recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = Recognizer.listen(mic)
+            text = Recognizer.recognize_bing(audio)
+            text= text.lower()
+
+            print(f"Recognized {text}")
+
+    except speech_recognition.UnknownValueError():
+        Recognizer= speech_recognition.Recognizer()
+        continue
